45ce9fa65a3b6a9535ffa4eff38c0f1fa1530761
WKJay <1931048074@qq.com>
2021-06-07 11:13:20 +0800

    add disk percentage display

--- src/js/domClass.js
+++ src/js/domClass.js
@@ -50,6 +50,18 @@ export class diskGroup {
             let used = disks[_i].total - disks[_i].free
             let usedPercentage = Math.ceil(used / disks[_i].total * 100)
             let _disName = disks[_i].name
+            let usedH = used
+            let totalH = disks[_i].total
+            let unitCnt = 0 // 0:B 1.KB 2:MB 3:GB
+            let unitChar = ['B', 'KB', 'MB', 'GB']
+            while (totalH > 1024) {
+                totalH = totalH / 1024
+                usedH = usedH / 1024
+                unitCnt++
+            }
+            totalH = totalH.toFixed(1)
+            usedH = usedH.toFixed(1)
+
             if (!this.rendered) {
                 if (this.isNameExist(_disName)) {
                     alert(_disName + "already exists")
@@ -58,6 +70,9 @@ export class diskGroup {
                     <div class="progressWrapper">
                         <span>
                             ${_disName}
+                        </span>
+                        <span class="diskPercentage" id="${_disName}Percentage">
+                        ${usedH}${unitChar[unitCnt]}/${totalH}${unitChar[unitCnt]}
                         </span>
                         <div class="progress">
                             <div class="progressbar" id="${_disName}Bar" style="width:${usedPercentage}%;">
@@ -68,9 +83,11 @@ export class diskGroup {
                     `)
                 }
             } else {
-                let _el = document.getElementById(_disName + 'Bar')
-                _el.style.width = `${usedPercentage}%`
-                _el.innerHTML = `&nbsp;${usedPercentage}%&nbsp;`
+                let _elProgressbar = document.getElementById(_disName + 'Bar')
+                let _eldiskPercentage = document.getElementById(_disName + 'Percentage')
+                _elProgressbar.style.width = `${usedPercentage}%`
+                _elProgressbar.innerHTML = `&nbsp;${usedPercentage}%&nbsp;`
+                _eldiskPercentage.innerHTML = `${usedH}${unitChar[unitCnt]}/${totalH}${unitChar[unitCnt]}`
             }
 
         }
